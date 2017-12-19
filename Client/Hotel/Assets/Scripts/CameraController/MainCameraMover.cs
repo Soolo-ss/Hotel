dbae67dd26aa17945ce5c82d59fe907bae9e1332
Soolo-ss <461273057@qq.com>
2017-12-19 19:48:42 +0800

    7
    7

---
+++ Client/Hotel/Assets/Scripts/CameraController/MainCameraMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraMover : MonoBehaviour {
+    public Transform target;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+        float zOffset = Mathf.Sqrt(offset / 3f);
+        Vector3 offsetVec = new Vector3(0, zOffset, offset);
+        this.transform.position = target.transform.position + offsetVec;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            this.transform.RotateAround(target.transform.position, Vector3.up, 90f);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            this.transform.RotateAround(target.transform.position, Vector3.up, -90f);
+        }
+
+        this.transform.LookAt(target);
+    }
+}
---
+++ Client/Hotel/Assets/Scripts/HeroController/HeroController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("isWork" + animator.GetBool("isWalk"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isWalk", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("isWalk", false);
+        }
+	}
+}
