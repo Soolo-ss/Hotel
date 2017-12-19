c6870d0d6c8563d6dcee6827c14f41a9c96769c5
Soolo-ss <461273057@qq.com>
2017-12-20 00:38:27 +0800

    9
    9

--- Client/Hotel/Assets/Scripts/HeroController/HeroController.cs
+++ Client/Hotel/Assets/Scripts/HeroController/HeroController.cs
@@ -4,27 +4,104 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour {
     private Animator animator;
-    private List<Sprite> heroSprites;
+	private List<KeyCode> runKey = new List<KeyCode>() { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+	private int moveSpeed = 1;
 
 	// Use this for initialization
 	void Start () {
         this.animator = GetComponent<Animator>();
-
-        heroSprites = new List<Sprite>(Resources.LoadAll<Sprite>("Sprites/Hero001"));
-
-        Debug.Log(heroSprites.Count);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.W))
-        {
-            animator.SetBool("isWalk", true);
-        }
 
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            animator.SetBool("isWalk", false);
-        }
+		move ();
+	
 	}
+
+	private bool GetRunKeyDown(){
+		bool isRun = false;
+		foreach (KeyCode key in runKey) {
+			if (Input.GetKeyDown (key)) {
+				isRun = true;
+				break;
+			}
+		}
+
+		return isRun;
+	}
+
+	private bool GetRunKeyUp(){
+		bool isUp = false;
+		foreach (KeyCode key in runKey) {
+			if (Input.GetKeyUp (key)) {
+				isUp = true;
+				break;
+			}
+		}
+
+		return isUp;
+	}
+
+	public void move()
+	{
+		if (GetRunKeyDown ()) {
+			animator.SetBool ("isRun", true);
+
+			if (Input.GetKeyDown (KeyCode.W)) {
+				moveUpClip ();
+			} else if (Input.GetKeyDown (KeyCode.S)) {
+				moveDownClip ();
+			} else if (Input.GetKeyDown (KeyCode.A)) {
+				moveLeftClip ();
+			} else if (Input.GetKeyDown (KeyCode.D)) {
+				moveRightClip ();
+			}
+		}
+
+		if (GetRunKeyUp()){
+			Debug.Log ("getrunkeyup");
+			animator.SetBool ("isRun", false);
+
+			if (Input.GetKeyUp (KeyCode.W)) {
+				moveUpClip ();
+			} else if (Input.GetKeyUp (KeyCode.S)) {
+				moveDownClip ();
+			} else if (Input.GetKeyUp (KeyCode.A)) {
+				moveLeftClip ();
+			} else if (Input.GetKeyUp (KeyCode.D)) {
+				moveRightClip ();
+			}
+		}
+	}
+
+	private void moveCalc() {
+		
+	}
+
+	private void moveUpClip() {
+		animator.SetFloat ("xdir", 0);
+		animator.SetFloat ("ydir", 1);
+	}
+
+	private void moveDownClip() {
+		animator.SetFloat ("xdir", 0);
+		animator.SetFloat ("ydir", -1);
+	}
+
+	private void moveLeftClip() {
+		animator.SetFloat ("xdir", -1);
+		animator.SetFloat ("ydir", 0);
+	}
+
+	private void moveRightClip() {
+		animator.SetFloat ("xdir", 1);
+		animator.SetFloat ("ydir", 0);
+	}
+
+	private void cancelClip() {
+		animator.SetFloat ("xdir", 0);
+		animator.SetFloat ("ydir", 0);
+	}
+
 }
