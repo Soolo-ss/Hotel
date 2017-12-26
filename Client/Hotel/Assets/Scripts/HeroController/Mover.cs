5d1c3bfd12a56809dee168f20f5451bc0f139412
Soolo-ss <461273057@qq.com>
2017-12-26 18:58:28 +0800

    19
    19

--- Client/Hotel/Assets/Scripts/HeroController/Mover.cs
+++ Client/Hotel/Assets/Scripts/HeroController/Mover.cs
@@ -14,18 +14,21 @@ public class Mover : MonoBehaviour {
 	private Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		this.animator = GetComponent<Animator>();
 		this.character = GetComponent<CharacterController> ();
 		this.mainCamera = Camera.main.transform;
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		move ();
 	}
 
-	void FixedUpdate() {
+	void FixedUpdate()
+    {
 		moveDirection = new Vector3 (-Input.GetAxis ("Horizontal"), 0, -Input.GetAxis ("Vertical"));
 		moveDirection = transform.TransformDirection (moveDirection);
 		moveDirection *= speed;
@@ -37,7 +40,8 @@ public class Mover : MonoBehaviour {
 		}
 	}
 
-	private bool GetRunKeyDown(){
+	private bool GetRunKeyDown()
+    {
 		bool isRun = false;
 		foreach (KeyCode key in runKey) {
 			if (Input.GetKeyDown (key)) {
@@ -49,22 +53,40 @@ public class Mover : MonoBehaviour {
 		return isRun;
 	}
 
-	private bool GetRunKeyUp(){
+	private bool GetRunKeyUp(ref KeyCode upKey)
+    {
 		bool isUp = false;
 		foreach (KeyCode key in runKey) {
 			if (Input.GetKeyUp (key)) {
 				isUp = true;
+                upKey = key;
 				break;
 			}
 		}
 
 		return isUp;
 	}
+
+    private bool isOtherRunKeyDown(KeyCode upKey)
+    {
+        bool isDown = false;
+
+        foreach(KeyCode key in runKey)
+        {
+            if (Input.GetKey(key) && key != upKey)
+            {
+                isDown = true;
+                break;
+            }
+        }
+
+        return isDown;
+    }
+
 	
 	public void move()
 	{
 		if (GetRunKeyDown ()) {
-            Debug.Log("getrunkeydown");
 			animator.SetBool ("isRun", true);
 
 			if (Input.GetKeyDown (KeyCode.W)) {
@@ -78,7 +100,8 @@ public class Mover : MonoBehaviour {
 			}
 		}
 
-		if (GetRunKeyUp()){
+        KeyCode upKey = new KeyCode(); ;
+		if (GetRunKeyUp(ref upKey) && !isOtherRunKeyDown(upKey)){
 			animator.SetBool ("isRun", false);
 
 			if (Input.GetKeyUp (KeyCode.W)) {
@@ -91,14 +114,7 @@ public class Mover : MonoBehaviour {
 				moveRightClip ();
 			}
 		}
-
-
 	}
-
-	private void moveCalc() {
-
-	}
-
 
 	private void moveUpClip() {
 		animator.SetFloat ("xdir", 0);
@@ -117,11 +133,6 @@ public class Mover : MonoBehaviour {
 
 	private void moveRightClip() {
 		animator.SetFloat ("xdir", 1);
-		animator.SetFloat ("ydir", 0);
-	}
-
-	private void cancelClip() {
-		animator.SetFloat ("xdir", 0);
 		animator.SetFloat ("ydir", 0);
 	}
 }
