b52c9293fc95662733633dfd4b906f466d1647d3
Soolo-ss <461273057@qq.com>
2018-01-02 23:21:59 +0800

    up
    up

---
+++ Client/Hotel/Assets/Scripts/UI/StartUIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartUIController : MonoBehaviour {
+
+    private Button startBtn;
+    private Button saveBtn;
+    private Button exitBtn;
+
+	// Use this for initialization
+	void Start () {
+        startBtn = this.transform.GetChild(1).GetComponent<Button>();
+        saveBtn = this.transform.GetChild(2).GetComponent<Button>();
+        exitBtn = this.transform.GetChild(3).GetComponent<Button>();
+
+        startBtn.onClick.AddListener(() =>
+        {
+            Debug.Log("load loading scence");
+            SceneManager.LoadScene(1);
+        });
+
+        saveBtn.onClick.AddListener(() =>
+        {
+            
+        });
+
+        exitBtn.onClick.AddListener(() =>
+        {
+
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
